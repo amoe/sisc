0ddbfaf30d304aaa709a921fabc20e8715305242
David Banks <amoebae@gmail.com>
2012-07-18 23:59:45 +0100

    throw a correct error if no arguments to command

--- src/sisc/modules/srfi/srfi-22/srfi-22.sh
+++ src/sisc/modules/srfi/srfi-22/srfi-22.sh
@@ -2,5 +2,11 @@
 SCHEMEENV=`basename $0`
 SOURCEFILE=$1
 
+if [ -z "$SOURCEFILE" ]; then
+    echo "$SCHEMEENV: cannot be used interactively." >&2
+    echo "Please provide a Scheme source file on the command line." >&2
+    exit 64    # EX_USAGE
+fi
+
 sisc -x -e "(srfi-22-prepare '$SCHEMEENV \"$SOURCEFILE\")" -c main-hook -- "$@"
 
